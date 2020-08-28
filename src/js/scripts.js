71ff2e511cf12e96353b0cb64c98968d41a5c43e
Eugene Samoilov <e.e.samoilov@mail.ru>
2020-08-28 12:21:20 +0400

    Uploading a finished project

---
+++ src/js/scripts.js
@@ -0,0 +1,56 @@
+window.addEventListener('DOMContentLoaded', function () {
+  
+  // modal window
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+        modal = document.querySelector('.overlay'),
+        modalCloseBtn = document.querySelector('[data-close]');
+
+  function openModal() {
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hiden';
+  }
+
+  modalTrigger.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
+
+  function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  }
+
+  modalCloseBtn.addEventListener('click', closeModal);
+
+  // close
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape" && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+  // open for scroll
+
+  function showModalByScroll() {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+      openModal();
+      window.removeEventListener('scroll', showModalByScroll);
+    }
+  }
+
+  window.addEventListener('scroll', showModalByScroll);
+
+  // burger menu
+
+  document.querySelector('.burger').addEventListener('click', function () {
+    document.querySelector('.navigation').classList.toggle('navigation_active');
+    document.querySelector('.burger__line').classList.toggle('active');
+  });
+});
